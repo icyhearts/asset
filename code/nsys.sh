bee431256e29b8b4248c3049e7a389f7549b0d20
like <kli@siorigin.com>
2026-01-29 16:02:01 +0800

    update

--- code/nsys.sh
+++ code/nsys.sh
@@ -1,14 +1,15 @@
 #~/opt/cuda-12.8/bin/ncu --set full --launch-count   --launch-skip --export temp 
 # --launch-count   1 --launch-skip  3
 set -x
+export CUDA_VISIBLE_DEVICES=6
 . /softhome/like/miniconda3/bin/activate simo_sglang
 which python
 cd /softhome/like/package/h100/package/sglang_kernel_src
-#export CUDA_VISIBLE_DEVICES=3; export bscale_dtype="";  /softhome/like/opt/cuda-12.8/bin/nsys profile --force-overwrite=true  -o temp/load_gptq_awq.$CUDA_VISIBLE_DEVICES.bscale_dtype-$bscale_dtype.nsys --trace='cuda,cublas,cudnn,nvtx,osrt,opengl'  python temp/load_gptq_awq.py --round 3 --warmup 2 --bscale_dtype "$bscale_dtype"
+export bscale_dtype=""
+/softhome/like/opt/cuda-12.8/bin/nsys profile --force-overwrite=true  -o temp/load_gptq_awq.$CUDA_VISIBLE_DEVICES.bscale_dtype-$bscale_dtype.nsys --trace='cuda,cublas,cudnn,nvtx,osrt,opengl'  python temp/load_gptq_awq.py --round 3 --warmup 2 --bscale_dtype "$bscale_dtype"
 nows=`date +%Y_%m_%d___%H_%M_%S`
-bash /share_data/users/like/bash-bin/bin/kgp.sh 6 likf &&  && CUDA_VISIBLE_DEVICES=6  MEMG=75 /share_data/users/like/package/h100/package/simo_conda_sglang/perf_bench > temp/out.log.$nows 2>&1 &
-
-/bin/bash /share_data/users/like/bash-bin/bin/kgp.sh 6 likf && CUDA_VISIBLE_DEVICES=6  MEMG=77 /share_data/users/like/package/h100/package/simo_conda_sglang/perf_bench  >  /share_data/users/like/package/h100/package/simo_conda_sglang/temp/out.log.$nows 2>&1 &
+export MEMG=77
+/bin/bash /share_data/users/like/bash-bin/bin/kgp.sh $CUDA_VISIBLE_DEVICES likf &&   &&    /share_data/users/like/package/h100/package/simo_conda_sglang/perf_bench  >  /share_data/users/like/package/h100/package/simo_conda_sglang/temp/out.log.$nows 2>&1 &
 
 
 
