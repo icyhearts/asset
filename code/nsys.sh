8293cca18357d4b0126990479784a864e991e73b
like <kli@siorigin.com>
2026-01-29 07:08:01 +0000

    update

--- code/nsys.sh
+++ code/nsys.sh
@@ -4,4 +4,4 @@ set -x
 . /softhome/like/miniconda3/bin/activate simo_sglang
 which python
 cd /softhome/like/package/h100/package/sglang_kernel_src
-export CUDA_VISIBLE_DEVICES=3; export bscale_dtype="";  /softhome/like/opt/cuda-12.8/bin/ncu --set full    --export temp/fused_moe_kernel_gptq_awq.$CUDA_VISIBLE_DEVICES.bscale_dtype-$bscale_dtype.ncu python temp/load_gptq_awq.py --round 3 --warmup 2 --bscale_dtype "$bscale_dtype"
+export CUDA_VISIBLE_DEVICES=3; export bscale_dtype="";  /softhome/like/opt/cuda-12.8/bin/nsys --force-overwrite=true     python temp/load_gptq_awq.py --round 3 --warmup 2 --bscale_dtype "$bscale_dtype"
