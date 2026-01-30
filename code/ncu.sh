39324824fe570bf5fe6fc1a2e2a763c7e5d51d09
like <kli@siorigin.com>
2026-01-30 11:20:02 +0800

    update

--- code/ncu.sh
+++ code/ncu.sh
@@ -7,7 +7,9 @@ cd /softhome/like/package/h100/package/sglang_kernel_src
 #export CUDA_VISIBLE_DEVICES=3; export bscale_dtype="";  /softhome/like/opt/cuda-12.8/bin/ncu --set full    --export temp/fused_moe_kernel_gptq_awq.$CUDA_VISIBLE_DEVICES.bscale_dtype-$bscale_dtype.ncu python temp/load_gptq_awq.py --round 3 --warmup 2 --bscale_dtype "$bscale_dtype"
 export CUDA_VISIBLE_DEVICES=4
 
-ncu --metrics dram__bytes_read.sum,lts__t_sector_hit_rate.pct,l1tex__t_sector_hit_rate.pct python temp/load_gptq_awq.py
+#ncu --metrics dram__bytes_read.sum,lts__t_sector_hit_rate.pct,l1tex__t_sector_hit_rate.pct python temp/load_gptq_awq.py
 
-export bscale_dtype="";  /softhome/like/opt/cuda-12.8/bin/ncu  --metrics  dram__bytes_read.sum,lts__t_sector_hit_rate.pct,l1tex__t_sector_hit_rate.pct \
+ 
+
+export bscale_dtype="";  /softhome/like/opt/cuda-12.8/bin/ncu  --metrics  sm__warps_active.avg.pct_of_peak_sustained_active,launch__registers_per_thread,launch__occupancy_limit_registers \
     python temp/load_gptq_awq.py --round 3 --warmup 2 --bscale_dtype "$bscale_dtype"
