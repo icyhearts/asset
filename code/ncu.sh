320c6f73a8e35cf97d139cac9539ac4a7c3cc9e8
like <kli@siorigin.com>
2026-01-30 11:22:01 +0800

    update

--- code/ncu.sh
+++ code/ncu.sh
@@ -5,7 +5,7 @@ set -x
 which python
 cd /softhome/like/package/h100/package/sglang_kernel_src
 #export CUDA_VISIBLE_DEVICES=3; export bscale_dtype="";  /softhome/like/opt/cuda-12.8/bin/ncu --set full    --export temp/fused_moe_kernel_gptq_awq.$CUDA_VISIBLE_DEVICES.bscale_dtype-$bscale_dtype.ncu python temp/load_gptq_awq.py --round 3 --warmup 2 --bscale_dtype "$bscale_dtype"
-export CUDA_VISIBLE_DEVICES=4
+export CUDA_VISIBLE_DEVICES=7
 
 #ncu --metrics dram__bytes_read.sum,lts__t_sector_hit_rate.pct,l1tex__t_sector_hit_rate.pct python temp/load_gptq_awq.py
 
