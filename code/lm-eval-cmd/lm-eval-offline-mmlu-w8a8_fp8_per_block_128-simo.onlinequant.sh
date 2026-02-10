f03652ea2310bb60481c1dc4b496facc6b1b7e79
like <kli@siorigin.com>
2026-02-10 11:42:01 +0800

    update

--- code/lm-eval-cmd/lm-eval-offline-mmlu-w8a8_fp8_per_block_128-simo.onlinequant.sh
+++ code/lm-eval-cmd/lm-eval-offline-mmlu-w8a8_fp8_per_block_128-simo.onlinequant.sh
@@ -5,9 +5,10 @@ nowstr=$(date +%Y_%m_%d___%H_%M_%S)
 export CUDA_VISIBLE_DEVICES=7
 export SIMO_SGLANG_REGISTER=1
 tp_size=1
+
 model_path="/data_gpu/models/share_data/modelzoo/weights/llm/deepseek/DeepSeekV2/DeepSeek-V2-Lite-Chat-16B_A2.4B/safetensor_weights/"
 config_file="quant_config_w8a8_fp8_per_block.json"
-model_args_base="\"pretrained\": \"${model_path}\", \"quantization\": \"simo\", \"json_model_override_args\": \"{\\\"quantization_config_file\\\": \\\"/share_data/users/like/package/h100/package/simo_conda_sglang/simo/extensions/vllm_simo/example/simo_quantization_config/online_quantization/${config_file}\\\"}\", \"tp_size\": ${tp_size}, \"dtype\": \"auto\", \"mem_fraction_static\": 0.75"
+model_args_base="\"pretrained\": \"${model_path}\", \"quantization\": \"simo\", \"json_model_override_args\": \"{\\\"quantization_config_file\\\": \\\"/share_data/users/like/package/h100/package/simo_conda_sglang/simo/extensions/sglang_simo/example/simo_quantization_config/online_quantization/${config_file}\\\"}\", \"tp_size\": ${tp_size}, \"dtype\": \"auto\", \"mem_fraction_static\": 0.75"
 lm_eval --model sglang \
     --model_args "{${model_args_base}}" \
     --tasks $task_name \
