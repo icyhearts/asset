2c532cd21eb776913561bed831f5f056966d53b4
like <kli@siorigin.com>
2026-05-25 23:56:03 +0800

    update

--- install-scripts-sgl/install-sglang.sh
+++ install-scripts-sgl/install-sglang.sh
@@ -1,4 +1,4 @@
 set -x
 LOG=temp/pip-sglang-log.main-local-dep.txt
 source install-scripts-sgl/env-build-pip.sh
-pip install --config-settings=build.verbose=true -vvv -e "python" --no-build-isolation > $LOG 2>&1 &
+pip install --config-settings=build.verbose=true -vvv -e "python" --no-build-isolation  --index-url https://download.pytorch.org/whl/cu128  --extra-index-url https://pypi.tuna.tsinghua.edu.cn/simple > $LOG 2>&1 &
