2257feb47ebae850882bb11dd5341f706977744f
like <kli@siorigin.com>
2026-05-25 16:08:02 +0800

    update

--- install-scripts-sgl/install-sglang.sh
+++ install-scripts-sgl/install-sglang.sh
@@ -1,4 +1,4 @@
 set -x
 LOG=temp/pip-sglang-log.main-local-dep.txt
-source like-useful/env-build-pip.sh
+source install-scripts-sgl/env-build-pip.sh
 pip install --config-settings=build.verbose=true -vvv -e "python" --no-build-isolation > $LOG 2>&1 &
