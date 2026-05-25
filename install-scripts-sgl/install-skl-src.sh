82060c61095084927c2158614ebf80dd5df2baf8
like <kli@siorigin.com>
2026-05-25 23:28:02 +0800

    update

--- install-scripts-sgl/install-skl-src.sh
+++ install-scripts-sgl/install-skl-src.sh
@@ -1,3 +1,4 @@
 set -x
 source install-scripts-sgl/env-build-pip.sh
-pip install --config-settings=build.verbose=true -vvv -e "sgl-kernel" --no-build-isolation > temp/pip-sgl-kernel.log.main-local-dep.txt 2>&1 &
+#pip install --config-settings=build.verbose=true -vvv -e "sgl-kernel" --no-build-isolation > temp/pip-sgl-kernel.log.main-local-dep.txt 2>&1 &
+pip install --config-settings=build.verbose=true -vvv -e "sgl-kernel" --no-build-isolation --index-url https://download.pytorch.org/whl/cu128  --extra-index-url https://pypi.tuna.tsinghua.edu.cn/simple > temp/pip-sgl-kernel.log.main-local-dep.txt 2>&1 &
