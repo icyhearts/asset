6ef6d8e62c162fced616cd841e9cff0a8b6de734
like <kli@siorigin.com>
2026-06-04 23:30:01 +0800

    update

--- code/like-useful-hpc-ops/deduct.cc
+++ code/like-useful-hpc-ops/deduct.cc
@@ -8,5 +8,25 @@ struct GroupGEMMFp8Config {
 
 # using SLayoutXAtom = decltype(slayout_selector<128, float_e4m3_t>());
   using SLayoutXAtom = decltype(slayout_selector<kSwizzleX, Tin>());
-    |
+    |{
+      #template <128,          float_e4m3_t, bool kKmajor = true>
+      template <int kSwizzle, typename T, bool kKmajor = true>
+      static constexpr auto slayout_selector()
+      // hit
+      if constexpr (kSwizzle == 128) {
+        if constexpr (kKmajor) {
+          #return cute::GMMA::Layout_K_SW128_Atom<float_e4m3_t>{};// hit
+          return cute::GMMA::Layout_K_SW128_Atom<T>{};// hit
+        }
+      }
+    }
 }
+现在:
+using SLayoutXAtom = cute::GMMA::Layout_K_SW128_Atom<float_e4m3_t>
+
+`3rd/cutlass/include/cute/atom/mma_traits_sm90_gmma.hpp:103-104`:
+
+#template <class float_e4m3_t>
+#using Layout_K_SW128_Atom = decltype(upcast<sizeof_bits<Type>::value>(Layout_K_SW128_Atom_Bits{}));
+template <class Type>
+using Layout_K_SW128_Atom = decltype(upcast<sizeof_bits<Type>::value>(Layout_K_SW128_Atom_Bits{}));
