d6efc8a2dde654a6afffc48c6f7378445944c3f4
like <kli@siorigin.com>
2026-06-19 17:00:03 +0800

    update

--- code/sync-code-gitlab.sh
+++ code/sync-code-gitlab.sh
@@ -5,3 +5,13 @@ git push -f gitlab main-local-dep
 # cutlass
 cd /data/like/package/cutlass/
 git push -f gitlab like
+
+# asset
+cd /softhome/like/asset && bash temp/add.sh && git commit -m "update"  && git push gitlab master
+
+# cute-gemm reed
+cd /data/like/package/cute-gemm
+git push -f gitlab like
+
+cd /data/like/package/hpc-ops
+git push -f gitlab like
