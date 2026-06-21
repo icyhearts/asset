2453840695872e861454d7d8a4f1728cc520dcb1
like <kli@siorigin.com>
2026-06-21 11:00:04 +0800

    update

--- code/cute-reed/like-useful/simulate.py
+++ code/cute-reed/like-useful/simulate.py
@@ -5,3 +5,25 @@ for m in range(8):
         swizzle = offset ^ (((offset & 0b111000000)) >> 3)
         line += f"{swizzle:3d} "
     print(line)
+
+
+
+
+for m in range(8):
+    line = ""
+    for n in range(32):
+        if n % 8 == 0:
+            offset = m * 32 + n
+            swizzle = offset ^ (((offset & 0b111000000)) >> 3)
+            line += f"{swizzle:3d} "
+    print(line)
+
+
+for m in range(8):
+    line = ""
+    for n in range(32):
+        if n % 8 == 0:
+            offset = m * 32 + n
+            swizzle = offset ^ (((offset & 0b111000000)) >> 3)
+            line += f"{swizzle//8:3d} "
+    print(line)
