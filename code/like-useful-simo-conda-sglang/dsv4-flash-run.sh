a85bdd2021474a77c237ab872b900fe79c061040
like <kli@siorigin.com>
2026-06-25 17:30:04 +0800

    update

--- code/like-useful-simo-conda-sglang/dsv4-flash-run.sh
+++ code/like-useful-simo-conda-sglang/dsv4-flash-run.sh
@@ -4,6 +4,8 @@ set -x
 SGLANG_LOGGING_CONFIG_PATH=/share_data/users/like/package/h100/package/sglang_kernel_src/like-useful/custom_sglang.simple.json SGLANG_JIT_DEEPGEMM_PRECOMPILE=0 CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES  sglang serve \
   --trust-remote-code \
   --model-path /data/like/hf-models/deepseek-v4-flash/ \
+  --mem-fraction-static 0.7 \
+   --cuda-graph-max-bs 16 \
   --log-level debug \
   --tp 4 \
   --moe-runner-backend marlin \
