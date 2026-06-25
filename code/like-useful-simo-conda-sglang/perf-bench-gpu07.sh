f1943790b65949341fecd5b3d06b2510cd9f6bb8
like <kli@siorigin.com>
2026-06-25 17:50:03 +0800

    update

--- code/like-useful-simo-conda-sglang/perf-bench-gpu07.sh
+++ code/like-useful-simo-conda-sglang/perf-bench-gpu07.sh
@@ -1,5 +1,5 @@
-memg=72
-CUDA_VISIBLE_DEVICES=0 SLMS=10 MEMG=$memg /share_data/users/like/package/h100/package/simo_conda_sglang/perf_bench  &
-CUDA_VISIBLE_DEVICES=4 SLMS=10 MEMG=$memg /share_data/users/like/package/h100/package/simo_conda_sglang/perf_bench  &
-CUDA_VISIBLE_DEVICES=6 SLMS=10 MEMG=$memg /share_data/users/like/package/h100/package/simo_conda_sglang/perf_bench  &
-CUDA_VISIBLE_DEVICES=7 SLMS=10 MEMG=$memg /share_data/users/like/package/h100/package/simo_conda_sglang/perf_bench  &
+memg=1
+CUDA_VISIBLE_DEVICES=0 SLMS=20 MEMG=$memg /share_data/users/like/package/h100/package/simo_conda_sglang/perf_bench  &
+CUDA_VISIBLE_DEVICES=2 SLMS=20 MEMG=$memg /share_data/users/like/package/h100/package/simo_conda_sglang/perf_bench  &
+CUDA_VISIBLE_DEVICES=4 SLMS=20 MEMG=$memg /share_data/users/like/package/h100/package/simo_conda_sglang/perf_bench  &
+CUDA_VISIBLE_DEVICES=5 SLMS=20 MEMG=$memg /share_data/users/like/package/h100/package/simo_conda_sglang/perf_bench  &
