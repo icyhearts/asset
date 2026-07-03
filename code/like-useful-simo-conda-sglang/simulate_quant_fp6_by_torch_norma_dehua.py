105d9dd718c03b8eb0fca2338e7bc79aaf8cef2e
like <kli@siorigin.com>
2026-07-03 18:20:05 +0800

    update

--- code/like-useful-simo-conda-sglang/simulate_quant_fp6_by_torch_norma_dehua.py
+++ code/like-useful-simo-conda-sglang/simulate_quant_fp6_by_torch_norma_dehua.py
@@ -7,9 +7,10 @@ F32_EXP_BIAS=127
 
 magic_adder = (1 << (F32_MBITS - mbits - 1)) - 1
 val_to_add = ((EXP_BIAS - F32_EXP_BIAS) << F32_MBITS) + magic_adder
-#x_pos_u32 = x_pos.to(tl.uint32, bitcast=True)
-x_pos_u32=(torch.tensor([3.5],dtype=torch.float32).view(torch.int32))
+#x_pos_u32=(torch.tensor([3.5],dtype=torch.float32).view(torch.int32))
+x_pos_u32=(torch.tensor([5.0],dtype=torch.float32).view(torch.int32))
 mant_odd = (x_pos_u32 >> (F32_MBITS - mbits)) & 1
+
 normal_x_u32 = x_pos_u32 + val_to_add
 normal_x_u32 += mant_odd
 normal_vals = normal_x_u32 >> (F32_MBITS - mbits)
