76905e907d4c0c470d2deefef10bea1ae3aef966
like <kli@siorigin.com>
2026-07-04 18:10:03 +0800

    update

--- code/like-useful-simo-conda-sglang/simulate_quant_fp6_by_torch_norma_dehua.py
+++ code/like-useful-simo-conda-sglang/simulate_quant_fp6_by_torch_norma_dehua.py
@@ -44,8 +44,10 @@ x_pos = torch.tensor([3.5],dtype=torch.float32)
 x_pos = torch.tensor([0x40690000],dtype=torch.int32).view(torch.float32)
 # 3) 平局, round to even, 但是我自己是even, mant_odd=0
 x_pos = torch.tensor([0x40680000],dtype=torch.int32).view(torch.float32)
-# 4) 平局
+# 4) 平局, mant_odd=1
 x_pos = torch.tensor([0x40580000],dtype=torch.int32).view(torch.float32)
+# 5) 不平局, 大于0.5, mant_odd=1
+x_pos = torch.tensor([0x40590000],dtype=torch.int32).view(torch.float32)
 
 print(f"x_pos:{x_pos}")
 x_pos_u32=x_pos.view(torch.int32)
