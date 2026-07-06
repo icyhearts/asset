a5cd69e49838451e627d81f7cd7b03679d591642
like <kli@siorigin.com>
2026-07-06 18:20:03 +0800

    update

--- code/like-useful-simo-conda-sglang/test-lstm.py
+++ code/like-useful-simo-conda-sglang/test-lstm.py
@@ -1,6 +1,12 @@
 from torch import nn
 rnn = nn.LSTM(10, 20, 2)
-print(f"rnn:{rnn}")
+print(f"rnn:{rnn}, rnn.input_size:{rnn.input_size}, rnn.hidden_size:{rnn.hidden_size}, rnn.num_layers:{rnn.num_layers}")
+>>> rnn.num_layers
+2
+>>> rnn.input_size
+rnn.input_size
+>>> rnn.hidden_size
+20
 
 input = torch.randn(5, 3, 10)
 h0 = torch.randn(2, 3, 20)
